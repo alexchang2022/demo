6c3bb53c61642de9aa5e6051eaa8d81e86e629b0
alexchang2022 <alexzhangsc@qq.com>
2022-05-25 13:33:20 +0800

    update truffle config

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,7 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Token {
-  constructor() public {
-  }
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title Token
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Based on https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.1/contracts/examples/SimpleToken.sol
+ */
+contract Token is ERC20 {
+
+    /*
+     * @dev Mint amount of every mint function.
+     */
+    uint256 constant MINT_AMOUNT = 100;
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) public ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    /*
+     * @dev Mint new tokens to caller
+     */
+    function mint() external {
+    	_mint(msg.sender, MINT_AMOUNT);
+    }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -18,10 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -41,11 +41,18 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://polygon-mumbai.g.alchemy.com/v2/6r22j0I40q8ZPxcCBVI1etmUPAZq2jiM`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
