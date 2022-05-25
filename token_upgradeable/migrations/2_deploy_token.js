52609b0955d0379c825cc4e52fb69255fb6d77f5
alexchang2022 <alexzhangsc@qq.com>
2022-05-26 02:38:51 +0800

    add async in migration file

--- token_upgradeable/migrations/2_deploy_token.js
+++ token_upgradeable/migrations/2_deploy_token.js
@@ -4,7 +4,7 @@ const Token = artifacts.require('Token');
 const TokenV1 = artifacts.require('TokenV1');
 
 module.exports = async function (deployer) {
-	deployProxy(Token, ["TokenV1", "BCD"], {deployer}).then(function(proxy) {
+	deployProxy(Token, ["TokenV1", "BCD"], {deployer}).then(async function(proxy) {
   		console.log('Proxy Deployed', proxy.address);
 		const proxy_new = await upgradeProxy(proxy.address, TokenV1, ["TokenV2", "GOLD"], {deployer});
 			console.log("Proxy New", proxy_new.address);
