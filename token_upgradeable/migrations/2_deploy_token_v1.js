d8b58ccaf978259875fb97d1d00099f353ae7333
alexchang2022 <alexzhangsc@qq.com>
2022-05-25 21:07:15 +0800

    update migration files

--- token_upgradeable/migrations/2_deploy_token_v1.js
+++ token_upgradeable/migrations/2_deploy_token_v1.js
@@ -3,6 +3,9 @@ const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 const TokenV1 = artifacts.require('TokenV1');
 
 module.exports = async function (deployer) {
-  const instance = await deployProxy(TokenV1, { deployer });
-  console.log('TokenV1 Deployed', instance.address);
+	deployer.deploy(TokenV1).then(function (token){
+  		console.log('TokenV1 Deployed', token.address);
+  		const proxy = await deployProxy(TokenV1, { deployer });
+  		console.log('Proxy Deployed', proxy.address);
+	});
 };
