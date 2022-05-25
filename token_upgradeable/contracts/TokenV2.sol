b027de2f9185893adbf6986391eac1dc3d1c510c
alexchang2022 <alexzhangsc@qq.com>
2022-05-25 23:21:19 +0800

    update migration

--- token_upgradeable/contracts/TokenV2.sol
+++ token_upgradeable/contracts/TokenV2.sol
@@ -5,15 +5,18 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "./TokenV1.sol";
 
-contract TokenV2 is TokenV1{
-    function initialize() initializer public {
-      TokenV1.initialize("TokenV2", "EFG");
+contract TokenV2 is Initializable, ERC20Upgradeable, UUPSUpgradeable, OwnableUpgradeable {
+    function initialize(string memory name, string memory symbol) initializer public {
+      __ERC20_init(name, symbol);
+      __Ownable_init();
+      __UUPSUpgradeable_init();
+
+      _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 
     /// @custom:oz-upgrades-unsafe-allow constructor
-    //constructor() initializer {}
+    constructor() initializer {}
 
-    //function _authorizeUpgrade(address) internal override onlyOwner {}
+    function _authorizeUpgrade(address) internal override onlyOwner {}
 }
--- token_upgradeable/migrations/2_deploy_token_v1.js
+++ token_upgradeable/migrations/2_deploy_token_v1.js
@@ -4,13 +4,13 @@ const TokenV1 = artifacts.require('TokenV1');
 const TokenV2 = artifacts.require('TokenV2');
 
 module.exports = async function (deployer) {
-	deployer.deploy(TokenV1, "TokenV1", "BCD").then(function (token){
+	deployer.deploy(TokenV1).then(function (token){
   		console.log('TokenV1 Deployed', token.address);
-		return deployProxy(TokenV1, {deployer}).then(function(proxy) {
+		return deployProxy(TokenV1, ["TokenV1", "BCD"], {deployer}).then(function(proxy) {
   			console.log('Proxy Deployed', proxy.address);
 			return deployer.deploy(TokenV2).then(async function (token2){
-  				console.log("TokenV2 Upgraded", token2.address);
-				const proxy_new = await upgradeProxy(proxy.address, TokenV2, {deployer});
+  				console.log("TokenV2 Deployed", token2.address);
+				const proxy_new = await upgradeProxy(proxy.address, TokenV2, ["TokenV2", "GOLD"], {deployer});
 				console.log("Proxy New", proxy_new.address);
 			});
 		});
