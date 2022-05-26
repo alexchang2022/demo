642abb91270544bc9ef1f591fbe2b89aee0ad022
alexchang2022 <alexzhangsc@qq.com>
2022-05-27 02:55:06 +0800

    add unitest for token upgradeable

--- token_upgradeable/test/index.js
+++ token_upgradeable/test/index.js
@@ -3,13 +3,18 @@ const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 const Token = artifacts.require('Token');
 const TokenV1 = artifacts.require('TokenV1');
 
-describe('upgrades', () => {
-  it('works', async (done) => {
-    this.timeout(10000);  //add timeout.
-    const token1 = await deployProxy(Token);
-    const token2 = await upgradeProxy(token1.address, TokenV1, "initializeV1");
+contract('Token', function(accounts){
+  let proxyInstance;
+  it('Upgradeable contract success', async () => {
+    proxyInstance = await deployProxy(Token, ["Token", "BCD"]);
+    console.log("	Proxy Address : %s", proxyInstance.address);
+    console.log("	Token Name: %s, Token Symbol: %s", await proxyInstance.name(), await proxyInstance.symbol());
 
-    const value = await token2.symbol();
-    assert.equal(value.toString(), 'GOLD');
+    proxyInstance = await upgradeProxy(proxyInstance.address, TokenV1, {kind:"uups", call:"initializeV1"});
+    console.log("	Proxy Address : %s", proxyInstance.address);
+    console.log("	New Token Name: %s, Token Symbol: %s", await proxyInstance.name(), await proxyInstance.symbol());
+
+    const value = await proxyInstance.symbol();
+    assert.equal(value.toString(), "GOLD");
   });
 });
